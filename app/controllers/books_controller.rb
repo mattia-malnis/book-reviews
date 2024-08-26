cefc0e99e3f5d0490431e704a88d108929091b85
Mattia Malnis <15941778+mattia-malnis@users.noreply.github.com>
2024-08-26 15:22:48 +0200

    Add basic book's show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
   include Pagy::Backend
 
+  before_action :find_book, only: [ :show ]
+
   def index
     if params[:query].present?
       @books = Book.search_books(params[:query])
@@ -9,5 +11,16 @@ class BooksController < ApplicationController
     end
 
     @pagy, @books = pagy(@books)
+  end
+
+  def show
+  end
+
+  private
+
+  def find_book
+    @book = Book.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render file: Rails.root.join("public", "404.html"), status: :not_found, layout: false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "books#index"
+
+  resources :books, only: [ :show ]
 end
