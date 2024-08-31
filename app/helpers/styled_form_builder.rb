aa424dde7c8cdd18451053bebe8314f2690459a3
Mattia Malnis <15941778+mattia-malnis@users.noreply.github.com>
2024-08-31 22:24:44 +0200

    Improve app styles

--- app/helpers/styled_form_builder.rb
+++ app/helpers/styled_form_builder.rb
@@ -8,7 +8,7 @@ class StyledFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def check_box(method, options = {})
-    default_class = "rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 mr-2"
+    default_class = "rounded border-gray-300 text-blue-600 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 mr-2"
     element_with_default_class(method, options, default_class) { super(method, options) }
   end
 
@@ -18,13 +18,13 @@ class StyledFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def submit(value = nil, options = {})
-    default_class = "w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    default_class = "w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
     element_with_default_class(value, options, default_class) { super(value, options) }
   end
 
   private
 
-  def element_with_default_class(method, options, default_class = "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50")
+  def element_with_default_class(method, options, default_class = "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50")
     options[:class] = options[:class] || default_class
     yield
   end
